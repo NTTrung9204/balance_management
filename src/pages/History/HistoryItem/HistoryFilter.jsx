b8cf649cd97b1da9e79d95d661b8724a54d1d134
Trung Nguyễn <trung9204@gmail.com>
2024-12-23 00:04:02 +0700

    feat: switch button history

--- src/pages/History/HistoryItem/HistoryFilter.jsx
+++ src/pages/History/HistoryItem/HistoryFilter.jsx
@@ -2,58 +2,39 @@ import React from 'react'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button';
 import DatePicker from '~/components/common/DatePicker'
+import { useState } from 'react';
 
 function HistoryFilter() {
+    const [type, setType] = useState('totalHistoryTransaction')
+
+    const handleSwitchButton = (e) => {
+        setType(e.target.id)
+    }
+
     return (
         <Box
             sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', flexWrap: 'wrap', gap: 2 }}
         >
             <Box
                 sx={{
-                    display: 'flex', gap: 2,
-                    flexDirection: {
-                        xs: 'column',
-                        sm: 'unset',
-                        md: 'unset',
-                        lg: 'unset'
-                    },
-                    // alignContent: {
-                    //     xs: 'center',
-                    //     sm: 'unset'
-                    // },
+                    display: 'flex', gap: 2, flexDirection: { xs: 'column', sm: 'unset', md: 'unset', lg: 'unset' },
                     flex: 1
-
                 }}
             >
-                <Button variant="outlined">
+                <Button id='totalHistoryTransaction' onClick={handleSwitchButton} variant={type === 'totalHistoryTransaction' ? "contained" : "outlined"}>
                     Total
                 </Button>
-                <Button variant="outlined" color='success'>
+                <Button id='incomeHistoryTransaction' onClick={handleSwitchButton} variant={type === 'incomeHistoryTransaction' ? "contained" : "outlined"} color='success'>
                     Cash inflow
                 </Button>
-                <Button variant="outlined" color='error'>
+                <Button id='outcomeHistoryTransaction' onClick={handleSwitchButton} variant={type === 'outcomeHistoryTransaction' ? "contained" : "outlined"} color='error'>
                     Cash outflow
                 </Button>
             </Box>
             <Box
                 sx={{
-                    display: {
-                        xs: 'flex',
-                        lg: 'unset'
-                    },
-                    justifyContent: {
-                        xs: 'center',
-                        lg: 'unset'
-                    },
-                    flex: {
-                        xs: 1,
-                        lg: 'unset'
-                    },
-                    minWidth: {
-                        xs: '300px',
-                        sm: '550px',
-                        lg: 'unset'
-                    }
+                    display: { xs: 'flex', lg: 'unset' }, justifyContent: { xs: 'center', lg: 'unset' },
+                    flex: { xs: 1, lg: 'unset' }, minWidth: { xs: '300px', sm: '550px', lg: 'unset' }
                 }}
             >
                 <DatePicker />
